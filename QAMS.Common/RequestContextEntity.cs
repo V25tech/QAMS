f5ebaa58eb92d6f9e200c8332d5d3b3e77597e8a
Sheikh <sheikh@V25Tech.com>
2024-05-29 15:22:19 +0530

    commonlibraryandapifoldernameschanged

---
+++ QAMS.Common/RequestContextEntity.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace QAMS.Common
+{
+
+    public class RequestContext
+    {
+
+        private int pagenumberField = 1;
+
+        private int pagesizeField = 9999;
+        public string? userName { get; set; }
+
+        public int PageNumber
+        {
+            get
+            {
+                return this.pagenumberField;
+            }
+            set
+            {
+                this.pagenumberField = value;
+            }
+        }
+
+        public int PageSize
+        {
+            get
+            {
+                return this.pagesizeField;
+            }
+            set
+            {
+                this.pagesizeField = value;
+            }
+
+        }       
+    }
+}
---
+++ QAMS.Common/ResponseContextConstants.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QAMS.Common
+{
+    public static class ResponseContextConstants
+    {
+        public const string TotalRows = "TotalRows";
+    }
+}
