174e5c8961a9958791f8d3569b16e055fd25b280
Sagar <Sagar@V25Tech.com>
2024-06-06 15:39:15 +0530

    pantid added to cc entity

--- QAMS.Entities/ChangeControlRegistration/ChangeControlRegistrationEntity.cs
+++ QAMS.Entities/ChangeControlRegistration/ChangeControlRegistrationEntity.cs
@@ -72,6 +72,8 @@ namespace QAMS.WEB.Entities
         public string qualityEvents { get; set; }
         public string initiatingDepartment { get; set; }
         public string typeOfChange { get; set; }
+
+        public string batchNoLotNo { get; set; }
         public string batchNoLotDetails { get; set; }
         public string dateofInitiation { get; set; }
         public string targetDateofClosure { get; set; }
