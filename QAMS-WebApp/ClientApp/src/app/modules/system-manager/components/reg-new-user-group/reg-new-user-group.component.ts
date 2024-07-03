6620524eb251e43f4768e7fd49e0c443c5538ecb
HP <HP@DESKTOP-RJCK03U>
2024-07-03 00:22:21 -0400

    binding users in usergroup

--- QAMS-WebApp/ClientApp/src/app/modules/system-manager/components/reg-new-user-group/reg-new-user-group.component.ts
+++ QAMS-WebApp/ClientApp/src/app/modules/system-manager/components/reg-new-user-group/reg-new-user-group.component.ts
@@ -5,6 +5,8 @@ import { Product } from 'src/app/models/product.model';
 import { UserGroupService } from '../../services/user-group.service';
 import { RegUserGroup, UserGroup } from 'src/app/models/userGroup.model';
 import { ActivatedRoute,Router } from '@angular/router';
+import { ModifyUserService } from '../../services/modify-user.service';
+import { ModifyUser } from 'src/app/models/modifyUser.model';
 
 @Component({
   selector: 'app-reg-new-user-group',
@@ -15,14 +17,17 @@ import { ActivatedRoute,Router } from '@angular/router';
 export class RegNewUserGroupComponent {
   userGroupForm!: FormGroup;
   id:number=0;
+  modifyUserDatasource: ModifyUser[]=[]; 
+  tmodifyUserDatasource: ModifyUser[]=[];    
   editMode: boolean = false;
   usergroupReg: RegUserGroup;
   editUserValue: RegUserGroup;
-  sourceProducts!: Product[];
-  targetProducts!: Product[];
+  //sourceProducts!: Product[];
+  //targetProducts!: Product[];
   constructor(private fb: FormBuilder,private router: Router,protected messageService:MessageService, private route: ActivatedRoute,
-    private UserGroupService: UserGroupService, private cdr: ChangeDetectorRef) { }
+    private UserGroupService: UserGroupService,private modifyUserService: ModifyUserService, private cdr: ChangeDetectorRef) { }
   ngOnInit() :void {
+    this.GetUserDetails();
     this.BuildUsergroupForm();
     this.cdr.detectChanges();
     this.route.queryParams.subscribe(params => {
@@ -31,10 +36,19 @@ export class RegNewUserGroupComponent {
       this.GetUsergroupDetailsbyId(this.id);
     })    
   }
+  GetUserDetails()
+  {
+    this.modifyUserService.getUserData().subscribe((data: any) => {
+      debugger;
+      this.modifyUserDatasource = data.response;
+      this.modifyUserDatasource.forEach(dataSource => (dataSource.createdDate = new Date(dataSource.createdDate)));      
+    }); 
+  }
 cancelClick(){
   this.router.navigateByUrl('/user-groups');
 }
 saveControlChange(userValue: RegUserGroup) {
+  debugger;
   this.UserGroupService.insertCustomerDetails(userValue).subscribe((data: any) => {    
     this.messageService.add({ severity: 'success', summary: 'Usergroup Registration Saved Successfull', detail: 'Message Content' });
     setTimeout(() => {
